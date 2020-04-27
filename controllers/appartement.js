526d84d8ca6481c209e11f5a1b8d388d1f5b2fdf
Carole SOTIRIO <ca.sotirio@gmail.com>
2020-04-27 17:29:01 +0200

    Liaison front et back - episode 1

--- controllers/appartement.js
+++ controllers/appartement.js
@@ -8,47 +8,29 @@ const Appartement = require("../models/Appartement");
 const controllerAppartement = {
   // Page 1 : Récupération info appartement (sans filtre)
   dataAppartement: (req, res) => {
-    Appartement.find({}, (err, data) => {
-      if (err) {
-        res.status(500).json({});
-        return;
+    Appartement.find(
+      {},
+      "nomApp descPage1App caracApp noteApp imgApp",
+      (err, data) => {
+        if (err) {
+          res.status(500).json({});
+          return;
+        }
+        res.json(data);
       }
-      let dataTable = [];
-      for (let i = 0; i < data.length; i++) {
-        let liste = {
-          nomApp: data[i].nomApp,
-          descPage1App: data[i].descPage1App,
-          caracApp: data[i].caracApp,
-          noteApp: data[i].noteApp,
-          imgApp: data[i].imgApp,
-        };
-        dataTable.push(liste);
-      }
-      res.json(dataTable);
-    });
+    );
   },
 
-  // Page 2 : Récupération info appartement (avec filtre de recherche nomApp)
+  // Page 2 : Récupération info appartement (avec filtre de recherche sur l'Id)
   dataAppartementDetails: (req, res) => {
-    let recherche = "Le Chambertin";
-    Appartement.find({ nomApp: recherche }, (err, data) => {
+    let recherche = req.query.id; //récupérer la variable depuis le front
+    console.log(recherche);
+    Appartement.findOne({ _id: recherche }, (err, data) => {
       if (err) {
         res.status(500).json({});
         return;
       }
-      let dataTable = [];
-      for (let i = 0; i < data.length; i++) {
-        let liste = {
-          nomApp: data[i].nomApp,
-          descPage2App: data[i].descPage2App,
-          equipApp: data[i].equipApp,
-          imgApp: data[i].imgApp,
-          prixNuit: data[i].prixNuit,
-          dateIndisp: data[i].dateIndisp,
-        };
-        dataTable.push(liste);
-      }
-      res.json(dataTable);
+      res.json(data);
     });
   },
 
@@ -59,15 +41,7 @@ const controllerAppartement = {
         res.status(500).json({});
         return;
       }
-      let dataTable = [];
-      for (let i = 0; i < data.length; i++) {
-        let liste = {
-          nomApp: data[i].nomApp,
-          imgApp: data[i].imgApp,
-        };
-        dataTable.push(liste);
-      }
-      res.json(dataTable);
+      res.json(data);
     });
   },
 };
--- controllers/reservation.js
+++ controllers/reservation.js
@@ -7,38 +7,18 @@ const Clients = require("../models/Clients");
 
 const controllerReservation = {
   dataReservationApp: (req, res) => {
-    Appartement.find({ nomApp: "Le Corton Charlemagne" }, (err, data) => {
+    Appartement.find(req, "nomApp prixNuit dateIndisp", (err, data) => {
       if (err) {
         res.status(500).json({});
         return;
       }
-      let dataTable = [];
-      for (let i = 0; i < data.length; i++) {
-        let liste = {
-          nomApp: data[i].nomApp,
-          prixNuit: data[i].prixNuit,
-          dateIndisp: data[i].dateIndisp,
-        };
-        dataTable.push(liste);
-      }
-      res.json(dataTable);
+      res.json(data);
     });
   },
 
   /*** Enregistre la demande de Réservation clients /POST/ ***/
   dataReservationClients: (req, res) => {
-    let donneesResa = {
-      nomApp: "Le Corton Charlemagne",
-      arrivee: "01/05/2020",
-      depart: "05/05/2020",
-      nbVoyageurs: 2,
-      totalBrut: 250,
-      fraisMenage: 20,
-      fraisService: 30,
-      taxeSejour: 40,
-      totalResa: 340,
-    };
-    Clients.insertMany(donneesResa, (err, res) => {
+    Clients.insertMany(req, (err, res) => {
       if (err) {
         console.error(err);
         res.status(500).json("Erreur d'enregistrement");
@@ -50,18 +30,7 @@ const controllerReservation = {
 
   /*** Récapitulatif Réservation Client /GET/ ***/
   dataConfirmationClients: (req, res) => {
-    let donneesResa = {
-      nomApp: "Le Corton Charlemagne",
-      arrivee: "01/05/2020",
-      depart: "05/05/2020",
-      nbVoyageurs: 2,
-      totalBrut: 250,
-      fraisMenage: 20,
-      fraisService: 30,
-      taxeSejour: 40,
-      totalResa: 340,
-    };
-    Clients.find(donneesResa, (err, res) => {
+    Clients.find(req, (err, res) => {
       if (err) {
         console.error(err);
         res.status(500).json("Erreur récapitulatif");
